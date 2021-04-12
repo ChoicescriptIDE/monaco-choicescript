18692d91bad0e9f8d7c125677692280be85e9e14
CSIDE-Shell <32689095+CSIDE-Shell@users.noreply.github.com>
2021-04-12 22:56:10 +0000

    Automatic build triggered by 73c5dce7adc70cd9560fcd7915939123aa0cb2de

--- release/min/monaco.contribution.js
+++ release/min/monaco.contribution.js
@@ -1,6 +1,6 @@
 /*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
- * monaco-css version: 0.0.1(743acf14548735279b500bc267c50909d5d0a79c)
+ * monaco-css version: 0.0.1(73c5dce7adc70cd9560fcd7915939123aa0cb2de)
  * Released under the MIT license
  * https://github.com/Microsoft/monaco-css/blob/master/LICENSE.md
  *-----------------------------------------------------------------------------*/
